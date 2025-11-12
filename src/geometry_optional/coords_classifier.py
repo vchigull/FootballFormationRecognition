88b1fb2b9b224afb8d98a89cbf822d2c52d90413
vchigull <144617157+vchigull@users.noreply.github.com>
2025-11-12 10:03:16 -0500

    Update coords_classifier.py

--- src/geometry_optional/coords_classifier.py
+++ src/geometry_optional/coords_classifier.py
@@ -1,1 +1,5 @@
-
+from src.geometry_optional.coords_classifier import Featurizer, CoordsClassifier
+feat = Featurizer(grid_w=5, grid_h=3).featurize(boxes, img.width, img.height, los_y)
+clf = CoordsClassifier()
+clf.fit([feat1, feat2, ...], [label1, label2, ...])
+y_hat = clf.predict(feat)
